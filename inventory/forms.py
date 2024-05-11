f565edb098655084607721f0158bbe80db396e2d
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2024-05-11 15:20:04 -0400

    working on functionality of changing profile picture

--- inventory/forms.py
+++ inventory/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from inventory.models import Product
+from inventory.models import UserProfile
 
 
 class UserRegistry(UserCreationForm):
@@ -23,4 +24,11 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ["artist", "location", "title", "description", "inventoryNum"]
+
+class UpdateProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+
+    class Meta:
+        model = UserProfile
+        fields = ['picture']
 
