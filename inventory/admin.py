e6c7de7dfdaca1d38823d8d733ddde3f817d9814
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2024-05-11 12:11:55 -0400

    created rules for admin.py

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,32 @@
+# inventory/admin.py
 from django.contrib import admin
+from inventory.models import Product, Order, UserProfile
 
-# Register your models here.
+admin.site.site_header = "Inventory Admin"
+
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ("artist", "location", "title")
+    list_filter = ["location"]
+    search_fields = ["artist"]
+
+
+# class OrderAdmin(admin.ModelAdmin):
+#     model = Order
+#     list_display = ("product", "created_by", "order_quantity", "date")
+#     list_filter = ["date"]
+#     search_fields = ["product"]
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    model = UserProfile
+    list_display = ("user", "physical_address", "mobile", "picture")
+    list_filter = ["user"]
+    search_fields = ["user"]
+
+
+admin.site.register(Product, ProductAdmin)
+# admin.site.register(Order, OrderAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
+
